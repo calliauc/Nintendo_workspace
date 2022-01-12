480f388f20e4b25e8f0f1713d5631553ccbd4b9a
pauls <sargespaul@gmail.com>
2022-01-12 14:48:36 +0100

    Création d'une boutique et de deux clients

--- NintendoProject/src/test/Test.java
+++ NintendoProject/src/test/Test.java
@@ -1,9 +1,13 @@
 package test;
 
+import model.Client;
+
 public class Test {
 
 	public static void main(String[] args) {
 	
+		Client c1 = new Client("LUIGI", "Mario");
+		Client c2 = new Client("ZELDA", "Link");
 
 	}
 
