8d25269668ef07feb6f7ab5fa5450069a8a99d9e
calliauc <clement@calliau.io>
2022-01-12 14:46:47 +0100

    Add 5 games and nintendo switch

--- NintendoProject/src/test/Test.java
+++ NintendoProject/src/test/Test.java
@@ -1,9 +1,20 @@
 package test;
 
+import model.Console;
+import model.Jeu;
+
 public class Test {
 
 	public static void main(String[] args) {
+		
+		Console nintendoSwitch = new Console("NintendoSwitch");
 	
+		Jeu marioKart = new Jeu("MarioKart", nintendoSwitch);
+		Jeu SmashBros = new Jeu("Super Smash Bros", nintendoSwitch);
+		Jeu pokemonUnite = new Jeu("Pokemon Unite", nintendoSwitch);
+		Jeu fireEmblem3H = new Jeu("Fire Emblem 3 Houses", nintendoSwitch);
+		Jeu xenobaldeChronicle = new Jeu("Xenobalde Chronicle", nintendoSwitch);
+
 
 	}
 
