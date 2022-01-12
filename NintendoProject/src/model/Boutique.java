6acc9c9ddac58a17249fe735c7df4faf2a974a70
AlanM <alan-mateu@hotmail.fr>
2022-01-12 14:42:24 +0100

    Update boutique and adresse

--- NintendoProject/src/model/Boutique.java
+++ NintendoProject/src/model/Boutique.java
@@ -2,4 +2,34 @@ package model;
 
 public class Boutique {
 
+	private String nom;
+	private String adresse;
+	
+	public Boutique(String nom, String adresse) {
+		this.nom = nom;
+		this.adresse = adresse;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "Boutique [nom=" + nom + ", adresse=" + adresse + "]";
+	}
+
 }
